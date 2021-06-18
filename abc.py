723e5eb5d9030e7d9c92e20adaa3331c3c5a2073
CloudedQuartz <ravenklawasd@gmail.com>
2021-06-18 09:32:41 +0530

    abc: add price calculator
    Signed-off-by: CloudedQuartz <ravenklawasd@gmail.com>

---
+++ abc.py
@@ -0,0 +1,39 @@
+# check if a rectangle is a square
+
+# simple (hopefully exception free) input function
+'''
+user_input_type(str, type):
+	Take input with input_text then convert to input_type.
+	if input does not match type, catch exception and
+	return None.
+	Returns "None" if -
+	1. input_text (first argument) is not str
+	2. usr_inp doesnt match out_type
+'''
+def user_input_type(input_text, out_type):
+	# Check input text type
+	if type(input_text) != str:
+		return
+
+	# assign to user_inp
+	usr_inp = input(input_text)
+
+	# try to convert to out_type, if exception return None.
+	try:
+		return out_type(usr_inp) 
+	except:
+		print("Please enter with proper type (%s)" %out_type)
+		return
+
+while 1:
+	print("Price calculator\n\tPrice of 1 unit = Rs.100")
+	qty = user_input_type("Quantity: ", int)
+	if qty == None:
+		print("Please enter a valid value")
+		pass
+
+	price = qty * 100
+	if price > 1000:
+		price = price * (9/10)
+		
+	print("price is", price)
